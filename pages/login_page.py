9e842eef66dea0b4c3383946f46098151b4d4465
wowfatima <wowfatima94@gmail.com>
2026-02-26 14:49:51 +0500

    added checking tabs_account_currency

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,6 +16,18 @@ class LoginPage:
         otp_inputs = self.page.locator("input[type='tel']")
         for i in range(len(otpcode)):
             otp_inputs.nth(i).fill(otpcode[i])
-
+            
+        
         self.page.fill("input[type='password']", password_director)
-        self.page.click("button[type='submit']")
+        #self.page.click("button[type='submit']")
+        self.page.get_by_text("Parolni unutdingizmi?").click()
+        otp_inputs = self.page.locator("input[type='tel']")
+        otp_inputs.first.wait_for()
+        for i in range(len(otpcode)):
+            otp_inputs.nth(i).fill(otpcode[i])
+        self.page.get_by_placeholder("Parol namunasi: SmartBank1").fill(password_director)
+        self.page.get_by_placeholder("Parolni qayta kiriting").fill(password_director)
+        self.page.mouse.wheel(0, 2000)
+        self.page.get_by_text('Davom etish').click()
+        self.page.fill("input[type='password']", password_director)
+        self.page.locator(".mantine-1ryt1ht").get_by_text("Kirish").click()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,17 +1,45 @@
 from pages.login_page import LoginPage
+from playwright.sync_api import expect
+
 
 def test_login(page):
     login = LoginPage(page)
 
     login.open()
     login.login()
-   
-    cancel_btn = page.get_by_test_id("onesignal-slidedown-cancel-button")
-    if cancel_btn.is_visible():
-        cancel_btn.click()
+    page.wait_for_load_state("networkidle")
+    page.locator("#onesignal-slidedown-cancel-button").click()
     page.locator(".mantine-1ryt1ht").get_by_text("Barcha hisoblar").click()
-    page.locator(".mantine-1ryt1ht").get_by_text("Barchasi").click()
     page.locator(".mantine-1ryt1ht").get_by_text("UZS").click()
+    amounts = page.locator(".amount")
+    for i in range(amounts.count()):
+        expect(amounts.nth(i)).to_contain_text("UZS")
+    
     page.locator(".mantine-1ryt1ht").get_by_text("USD").click()
+    amounts = page.locator(".amount")
+    for i in range(amounts.count()):
+        expect(amounts.nth(i)).to_contain_text("USD")
+
     page.locator(".mantine-1ryt1ht").get_by_text("RUB").click()
+    amounts = page.locator(".amount")
+    for i in range(amounts.count()):
+        expect(amounts.nth(i)).to_contain_text("RUB")
+    
+
+
+
+
+
+   
+    # page.locator(input["type='button'"]).get_by_text("UZS").click()
+    # page.wait
+
+    # page.locator(input["type='button'"]).get_by_text("USD").click()
+    # page.wait
+
+    # page.locator(input["type='button'"]).get_by_text("RUB").click()
+    # page.wait
+
+
+    
 
